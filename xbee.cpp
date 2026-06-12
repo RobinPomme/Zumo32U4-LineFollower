77d8534e7bcfe008f204aa28a9c7fea40a0f0b3e
Robin Pommé <25095781@student.hhs.nl>
2026-06-12 15:48:55 +0200

    Most recent version of xbee into main.

---
+++ xbee.cpp
@@ -0,0 +1,13 @@
+#include "xbee.h"
+
+Xbee::Xbee() {
+    Serial1.begin(9600); //start serial communication via de XBee
+}
+
+char Xbee::leesXbee() {
+    return Serial1.read(); //leest de XBee af
+}
+
+void Xbee::printLineBreak() {
+    printXbee("------------------------------------------");
+}
