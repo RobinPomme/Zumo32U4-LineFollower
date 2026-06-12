042f9763043c7d12407ee3b4d161336731956d2f
Robin Pommé <25095781@student.hhs.nl>
2026-06-12 16:03:16 +0200

    Added main.ino, still w.i.p.

---
+++ main.ino
@@ -0,0 +1,49 @@
+#include "rijden.h"
+#include "xbee.h"
+#include "lijn.h"
+
+Xbee xbee;  //maak een nieuw object xbee van klasse Xbee
+Zumo32U4ButtonB knopB;
+Zumo32U4ButtonA knopA;
+Zumo32U4ButtonC knopC;
+LijnSensor lijnSensor(&xbee);
+Rijden Motors(&xbee, &lijnSensor);
+bool pauzeTijd;
+bool blokjesTijd;
+
+
+void setup() {
+  Serial.begin(9600);  //start serial output via de kabel (deze wordt afgebroken zodra de kabel is losgekoppend (daar is de xbee voor))
+  delay(1000);
+  Serial.println("De kabel kan er nu uit getrokken worden.");
+  xbee.printLineBreak();
+  xbee.printXbee("Om te beginnen, druk op knop b");
+  xbee.printLineBreak();
+  knopB.waitForButton();
+  Motors.initialiseer();  //initialiseert de motoren (doet niet heel veel de zumo draait naar links, naar rechts en terug naar het midden)
+  lijnSensor.kalibreerAlles();
+  pauzeTijd = true;
+  blokjesTijd = false;
+}
+
+void loop() {
+  if (knopA.isPressed()) {
+    pauzeTijd = true;
+  }
+
+  if (knopC.isPressed()) {
+    pauzeTijd = false;
+  }
+
+  if (!pauzeTijd) {
+    int lijnPositie = lijnSensor.leesPositie();
+
+    if(blokjesTijd) {
+
+    } else {
+      Motors.pidController(lijnPositie);
+    }
+  } else {
+    Motors.Stop();
+  }
+}
