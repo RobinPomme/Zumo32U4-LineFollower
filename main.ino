f5e0f5dc2fd786a24679a2d05ce921c9b464ce53
Robin Pommé <25095781@student.hhs.nl>
2026-06-19 15:45:13 +0200

    main.ino update.

--- main.ino
+++ main.ino
@@ -1,16 +1,26 @@
 #include "rijden.h"
 #include "xbee.h"
 #include "lijn.h"
+#include "proximity.h"
+#include "encoder.h"
+#include "blokZoeker.h"
 
-Xbee xbee;  //maak een nieuw object xbee van klasse Xbee
-Zumo32U4ButtonB knopB;
+// Maak een object aan voor alle drie de knoppen op de Zumo
 Zumo32U4ButtonA knopA;
+Zumo32U4ButtonB knopB;
 Zumo32U4ButtonC knopC;
+
+// 
+Xbee xbee;
+// 
 LijnSensor lijnSensor(&xbee);
 Rijden Motors(&xbee, &lijnSensor);
+ProximitySensor proximitySensor(&xbee);
+Encoder encoder;
+BlokZoeker blokZoeker(&proximitySensor, &Motors, &lijnSensor, &encoder);
+
 bool pauzeTijd;
 bool blokjesTijd;
-
 
 void setup() {
   Serial.begin(9600);  //start serial output via de kabel (deze wordt afgebroken zodra de kabel is losgekoppend (daar is de xbee voor))
@@ -38,8 +48,13 @@ void loop() {
     int lijnPositie = lijnSensor.leesPositie();
 
     if(blokjesTijd) {
-      // motor stop
-      //rijdnaarmidden()
+      xbee.printXbee("Blokjestijd True");
+      blokZoeker.rijNaarMidden();
+      xbee.printXbee("Midden reached");
+      blokZoeker.zoekBlok();
+      xbee.printXbee("We gaan duwe :)");
+      blokZoeker.duwBlok(&xbee);
+      pauzeTijd = true;
     } else {
       Motors.pidController(lijnPositie);
     }
